8bea8564f37b95ffc446ed5a9ce66155b691518b
eliascrapa <eliascrapa@googlemail.com>
2024-03-14 12:54:04 -0700

    adding SoupX

--- Scripts/Analysis_SN.R
+++ Scripts/Analysis_SN.R
@@ -11,6 +11,7 @@ library(rafalib)
 library(DoubletFinder)
 library(kableExtra)
 library(ggplot2)
+library(SoupX)
 
 #instseurat.packages("Matrix", type = "source")
 #install.packages("irlba", type = "source")
@@ -18,49 +19,194 @@ library(ggplot2)
 analysisfolder <- here::here("RNA")
 setwd(analysisfolder)
 
-data.dir <- "/Users/eliascrapa/Library/CloudStorage/Box-Box/Rat SAN different region snRNA seq/SN/Input"
-
-# Load the library
-library(Seurat)
-
 # Define the file paths and Read data
-data1 <- Read10X_h5("/Users/eliascrapa/Library/CloudStorage/Box-Box/Rat SAN different region snRNA seq/SN/Input/RATSANcluster3/filtered_feature_bc_matrix.h5")
-data2 <- Read10X_h5("/Users/eliascrapa/Library/CloudStorage/Box-Box/Rat SAN different region snRNA seq/SN/Input/RATSANcluster4/filtered_feature_bc_matrix.h5")
+data1 <- Read10X_h5(paste0(here::here("Input"),"/RATSANcluster3/filtered_feature_bc_matrix.h5"))
+data2 <- Read10X_h5(paste0(here::here("Input"),"/RATSANcluster4/filtered_feature_bc_matrix.h5"))
+
+
+
+
+sampleinputlist <- list()
+
+sampleinputlist <- as.list(c("RATSANcluster3","RATSANcluster4"))
+#df <- readRDS(file=here("ATAC","Plots",paste0("embeddings.Rds")))
+
+for (k in sampleinputlist) {
+  
+  #Adding Counts and ATAC Data TNT
+  data.dir <- paste0(k)
+  dir.create(here("RNA",paste0("/",data.dir)))
+  dir.create(here("RNA","RData"))
+  #Soup-X Contamination Filtering  
+  # Load data and estimate soup profile
+  # extract RNA 
+  
+  
+  
+  
+  tod = Seurat::Read10X_h5(paste0(here::here("Input"),"/",k,"/raw_feature_bc_matrix.h5"))
+  toc = Seurat::Read10X_h5(paste0(here::here("Input"),"/",k,"/filtered_feature_bc_matrix.h5"))
+  sc = SoupChannel(tod,toc)
+   
+  #sc = load10X(here(paste0("../Joined12w_Swimming_CellrangerOutput/full/",k,"/outs")))
+  
+  
+  #    Samplecluster <- df %>% filter(Sample == k) %>% dplyr::select(Barcode,Cluster) #read.csv(paste0("./",k,"/analysis/clustering/gex/graphclust/clusters.csv"))
+  # Samplecluster$Cluster <- as.numeric(Samplecluster$Cluster)
+  
+  #Samplecluster <- read.csv(here::here(paste0("Input",k,"/outs/analysis/clustering/gex/graphclust/clusters.csv")))
+  Samplecluster <- read.csv(here::here(paste0("Input",k,"/outs/analysis/clustering/gex/graphclust/clusters.csv")))
+  sc = setClusters(sc, setNames(Samplecluster$Cluster,Samplecluster$Barcode)) #setNames(clusters, barcodes) 
+  
+  
+  # #adding ArchrUMAP - problem is that there are less cells since they are already filtered  
+  # UMAPGEX <- df %>% dplyr::select(Barcode,`LSI_Combined.UMAP_Dimension_1`,`LSI_Combined.UMAP_Dimension_2`)#read.csv(paste0("./",k,"/analysis/dimensionality_reduction/gex/umap_projection.csv"))
+  # #UMAPGEX$Cluster <- df$Cluster
+  # colnames(UMAPGEX) <- c("Barcode","UMAP.1","UMAP.2")
+  # UMAPGEX$Annotation <- UMAPGEX$Cluster
+  # sc = setDR(sc, UMAPGEX[,c("UMAP.1","UMAP.2")])
+  
+  
+  UMAPGEX <- read.csv(here(paste0("../Joined12w_Swimming_CellrangerOutput/full/",k,"/outs/analysis/dimensionality_reduction/gex/umap_projection.csv")))
+  UMAPGEX$Cluster <- Samplecluster$Cluster
+  UMAPGEX$Annotation <- Samplecluster$Cluster
+  sc = setDR(sc, UMAPGEX[,c("UMAP.1","UMAP.2")])
+  
+  #visual sanity check
+  library(ggplot2)
+  dd = UMAPGEX#[colnames(sc$toc), ]
+  mids = aggregate(cbind(UMAP.1,UMAP.2) ~ Annotation, data = dd, FUN = mean)
+  gg = ggplot(dd, aes(UMAP.1, UMAP.2)) + geom_point(aes(colour = Annotation), size = 0.2) + ggtitle("TNT") + 
+    guides(colour = guide_legend(override.aes = list(size = 1)))
+  plot(gg)
+  
+  gg = ggplot(dd, aes(UMAP.1, UMAP.2))+ geom_point(size = 0.2)
+  plot(gg)
+  
+  dd$Tnnt2 = sc$toc["Tnnt2", ]
+  gg = ggplot(dd, aes(UMAP.1, UMAP.2)) + geom_point(aes(colour = Tnnt2 > 70))
+  plot(gg)
+  dd$'Hbb-bs' = sc$toc["Hbb-bs", ]
+  gg = ggplot(dd, aes(UMAP.1, UMAP.2)) + geom_point(aes(colour = 'Hbb-bs' > 0))
+  plot(gg)
+  
+  gg = plotMarkerMap(sc, "Tnnt2", pointSize = 0.5)
+  plot(gg)
+  gg = plotMarkerMap(sc, "Hbb-bs", pointSize = 0.5)
+  plot(gg)
+  gg = plotMarkerDistribution(sc)
+  plot(gg)
+  
+  print(paste0(k))
+  png(paste0(analysisfolder,"/",k,"/Soupestimation_",k,".png"))
+  sc = autoEstCont(sc)
+  dev.off()
+  
+  #sc = setContaminationFraction(sc, 0.2)
+  #Genes with highest expression in background.
+  BackgroundgenesSample <- head(sc$soupProfile[order(sc$soupProfile$est, decreasing = T), ], n = 100)
+  write.csv(BackgroundgenesSample, file = paste0(analysisfolder,"/",k,"/Backgroundgenes_",k,".csv"))
+  
+  
+  backgroundlist <- list()
+  backgroundlist <- as.list(head(rownames(BackgroundgenesSample), n = 25))
+  
+  
+  #adjust Counts based on SoupX, save Counts to SparseMatrix and H5-file
+  adj.rna_counts <- adjustCounts(sc, roundToInt = T)
+  
+  
+  DropletUtils:::write10xCounts(paste0("./",k,"/soupX_TNT_filt"), adj.rna_counts,
+                                overwrite = TRUE)
+  
+  DropletUtils:::write10xCounts(paste0("./",k,"/soupX_TNT_filt.h5"), adj.rna_counts, type = ("HDF5"),
+                                #barcodes = colnames(x),
+                                #gene.id = rownames(x),
+                                #gene.symbol = gene.id,
+                                #gene.type = "Gene Expression",
+                                overwrite = TRUE,
+                                genome = "MM10",
+                                version = ("3"),
+                                chemistry = "10x Multiome v1",
+                                original.gem.groups = '4d4',
+                                library.ids = "TNT"
+  )
+  
+  
+  for (l in backgroundlist){
+    png(paste0(analysisfolder,"/",k,"/",l,"_ChangeExpressionSoupX.png"), width = 1000, height = 800, units = "px")
+    print(l)
+    gg = plotChangeMap(sc, adj.rna_counts, paste(l))
+    plot(gg)
+    dev.off()
+    
+    
+  }
+  
+  
+  inputdata.10x <- Read10X_h5(here(paste0("../Joined12w_Swimming_CellrangerOutput/Swimming/",k,"/filtered_feature_bc_matrix.h5")))
+  # extract RNA and ATAC data
+  #rna_counts <- inputdata.10x$`Gene Expression`
+  atac_counts <- inputdata.10x$Peaks
+  rm(inputdata.10x)
+  
+  # Create Seurat object
+  assign(paste0(k),CreateSeuratObject(counts = adj.rna_counts))
+  
+  
+  
+  rm(backgroundlist)
+  rm(dd)
+  rm(gg)
+  rm(l)
+  rm(Samplecluster)
+  rm(sc)
+  rm(mids)
+  rm(UMAPGEX)
+  rm(atac_counts)
+  rm(adj.rna_counts)
+
+  
+}                   
+rm(k)  
+
+
+
+
 # Create Seurat objects
 seurat_object1 <- CreateSeuratObject(counts = data1, project = "RATSANcluster3")
 seurat_object2 <- CreateSeuratObject(counts = data2, project = "RATSANcluster4")
 
 
 
-seurat_object1 <- CreateSeuratObject(counts = data1, project = "RATSANcluster3")
-seurat_object2 <- CreateSeuratObject(counts = data2, project = "RATSANcluster4")
 
+#Define a function to preprocess and apply DoubletFinder
+preprocess_and_remove_doublets <- function(seurat, pN = 0.25, pK.set, nExp_poi) {
+  seurat <- NormalizeData(seurat)
+  seurat <- FindVariableFeatures(seurat, selection.method = "vst", nfeatures = 2000)
+  seurat <- ScaleData(seurat, features = rownames(seurat))
+  seurat <- RunPCA(seurat, features = VariableFeatures(object = seurat))
+  seurat <- FindNeighbors(seurat, dims = 1:10)
+  seurat <- FindClusters(seurat, resolution = 0.5)
+  seurat <- RunUMAP(seurat, dims = 1:10)
+  
+  sweep.res <- paramSweep(seurat, PCs = 1:20, sct = FALSE)
+  sweep.stats <- summarizeSweep(sweep.res, GT = FALSE)
+  seurat <- doubletFinder(seurat, PCs = 1:20, pN = pN, pK = as.numeric(as.character(pK.set)), nExp = nExp_poi, reuse.pANN = FALSE, sct = FALSE)
+  seurat <- subset(seurat, DF.classifications_0.25_0.02_142 == "Singlet")
+  
+  return(seurat)
+}
 
-
-
-
-
-seurat <- NormalizeData(seurat)
-seurat <- FindVariableFeatures(seurat, selection.method = "vst", nfeatures = 2000)
-seurat <- ScaleData(seurat, features = rownames(seurat))
-seurat <- RunPCA(seurat, features = VariableFeatures(object = seurat))
-seurat <- FindNeighbors(seurat, dims = 1:10)
-seurat <- FindClusters(seurat, resolution = 0.5)
-seurat <- RunUMAP(seurat, dims = 1:10)
-ElbowPlot(seurat)
-
-#######DOUBLETREMOVAL    
-sweep.res <- paramSweep(seurat, PCs = 1:20, sct = FALSE)
-sweep.stats <- summarizeSweep(sweep.res, GT = FALSE)
-bcmvn <- find.pK(sweep.stats) # should return NULL??
-pK.set <- unique(sweep.stats$pK)[2]
-nExp_poi <- round(0.08*nrow(seurat@meta.data))
-seurat <- doubletFinder(seurat, PCs = 1:20, pN = 0.25, pK = as.numeric(as.character(pK.set)), nExp = nExp_poi, reuse.pANN = FALSE, sct = FALSE)
-seurat <- subset(seurat,  DF.classifications_0.25_0.02_142 == "Singlet")
+# Apply the function to both Seurat objects
+seurat_object1 <- preprocess_and_remove_doublets(seurat_object1, pN = 0.25, pK.set = unique(sweep.stats$pK)[2], nExp_poi = round(0.08*nrow(seurat_object1@meta.data)))
+seurat_object2 <- preprocess_and_remove_doublets(seurat_object2, pN = 0.25, pK.set = unique(sweep.stats$pK)[2], nExp_poi = round(0.08*nrow(seurat_object2@meta.data)))
 
 # Merge the two Seurat objects into one
 seurat <- merge(seurat_object1, y = seurat_object2)
+
 rm(data1,data2,seurat_object1,seurat_object2)
+
 
 
 
